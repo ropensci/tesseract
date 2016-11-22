7f94e3841fe7d9c5ff7b8a6991902a8966280c92
Jeroen Ooms <jeroenooms@gmail.com>
2016-11-22 16:44:30 +0100

    Try Warning instead

--- R/onload.R
+++ R/onload.R
@@ -4,6 +4,6 @@
     Sys.setenv(TESSDATA_PREFIX = tessdata)
   }
   tryCatch(tesseract(), error = function(e){
-    stop("Unable to find English training data, please install: apt-get install tesseract-ocr-eng", call. = FALSE)
+    warning("Unable to find English training data, please install: apt-get install tesseract-ocr-eng", call. = FALSE)
   })
 }
