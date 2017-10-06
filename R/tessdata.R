a6d10042302e2683f594dbdaad8336e6ae1b7404
Jeroen <jeroenooms@gmail.com>
2017-10-06 22:55:04 +0200

    Prepare for Tesseract 4

--- R/tessdata.R
+++ R/tessdata.R
@@ -26,7 +26,8 @@ tesseract_download <- function(lang, datapath = NULL, progress = TRUE){
   }
   stopifnot(is.character(lang))
   stopifnot(is.character(datapath))
-  url <- sprintf('https://github.com/tesseract-ocr/tessdata/raw/3.04.00/%s.traineddata', lang)
+  branch <- ifelse(as.numeric_version(tesseract_config()$version) < 4, "3.04.00", "4.00")
+  url <- sprintf('https://github.com/tesseract-ocr/tessdata/raw/%s/%s.traineddata', branch, lang)
   req <- curl::curl_fetch_memory(url, curl::new_handle(
     noprogress = !isTRUE(progress),
     progressfunction = progress_fun
