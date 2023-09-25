5cfa8a76f1accb933888ee45fd153bdacbceeb3a
Jeroen Ooms <jeroenooms@gmail.com>
2023-09-25 19:20:55 +0200

    Fix clang

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -2,6 +2,8 @@ if(!file.exists("../windows/tesseract/include/tesseract/baseapi.h")){
   unlink("../windows", recursive = TRUE)
   url <- if(grepl("aarch", R.version$platform)){
     "https://github.com/r-windows/bundles/releases/download/tesseract-5.3.2/tesseract-ocr-5.3.2-clang-aarch64.tar.xz"
+  } else if(grepl("clang", Sys.getenv('R_COMPILED_BY'))){
+    "https://github.com/r-windows/bundles/releases/download/tesseract-5.3.2/tesseract-ocr-5.3.2-clang-x86_64.tar.xz"
   } else if(getRversion() >= "4.3") {
     "https://github.com/r-windows/bundles/releases/download/tesseract-5.3.2/tesseract-ocr-5.3.2-ucrt-x86_64.tar.xz"
   } else {
