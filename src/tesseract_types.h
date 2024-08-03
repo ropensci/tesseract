156873be1cf9207754b7f4236b3b83e36cab2efc
Mauricio 'Pacha' Vargas Sepulveda <m.sepulveda@mail.utoronto.ca>
2024-08-03 13:39:43 -0400

    small PR to use cpp11

--- src/tesseract_types.h
+++ src/tesseract_types.h
@@ -1,14 +1,29 @@
-#include <tesseract/baseapi.h>
 #include <leptonica/allheaders.h>
+#include <tesseract/baseapi.h>
 
 #define R_NO_REMAP
 #define STRICT_R_HEADERS
 
-#include <Rcpp.h>
+#include <cpp11.hpp>
 
-inline void tess_finalizer(tesseract::TessBaseAPI *engine) {
+inline void tess_finalizer(tesseract::TessBaseAPI* engine) {
   engine->End();
   delete engine;
 }
 
-typedef Rcpp::XPtr<tesseract::TessBaseAPI, Rcpp::PreserveStorage, tess_finalizer, true> TessPtr;
+typedef cpp11::external_pointer<tesseract::TessBaseAPI, tess_finalizer> TessPtr;
+
+inline void set_tesseract_options(tesseract::TessBaseAPI* engine,
+                                  cpp11::list options) {
+  for (int i = 0; i < options.size(); ++i) {
+    std::string key = cpp11::as_cpp<std::string>(options.names()[i]);
+    std::string value = cpp11::as_cpp<std::string>(options[i]);
+    engine->SetVariable(key.c_str(), value.c_str());
+  }
+}
+
+inline TessPtr make_tess_ptr(tesseract::TessBaseAPI* engine,
+                             cpp11::list options = cpp11::list()) {
+  set_tesseract_options(engine, options);
+  return TessPtr(engine);
+}
